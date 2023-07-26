6a5d6ef6eb73cbefbb122e60ec6c4cf0995577cb
meroneyeram <meroneyeram@gmail.com>
2023-07-26 09:04:23 -0700

    Added update service and updateProduct handler

--- src/main/java/com/org/walkapp/controller/ProductController.java
+++ src/main/java/com/org/walkapp/controller/ProductController.java
@@ -5,6 +5,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,6 +28,10 @@ public class ProductController {
 	}
 	
 	
-	
+	@PutMapping("/product/{id}")
+	public ResponseEntity<Integer> udpateProduct(@PathVariable("id") Long id, @RequestBody Product request) {
+		
+		return new ResponseEntity<Integer>(service.update(id, request), HttpStatus.OK);
+	}
 
 }
--- src/main/java/com/org/walkapp/services/WalkingService.java
+++ src/main/java/com/org/walkapp/services/WalkingService.java
@@ -1,5 +1,6 @@
 package com.org.walkapp.services;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
@@ -9,6 +10,8 @@ import org.springframework.stereotype.Service;
 import com.org.walkapp.entities.Product;
 import com.org.walkapp.entities.dao.ProductCategoryDao;
 import com.org.walkapp.entities.dao.ProductDao;
+
+import jakarta.persistence.Column;
 
 
 @Service
@@ -26,9 +29,86 @@ public class WalkingService {
 				    
 			}
 			
-//			public List<Product> findByCategoryId(Long id) {
-//			    // find it by category id
-//		    }
 			
+			public Integer update(Long id, Product request) {
+				Product product = pdao.findById(id).get();
+				if(product != null) {
+					product.setCategory(request.getCategory());
+					product.setSku(request.getSku());
+					product.setName(request.getName());
+					product.setDescription(request.getDescription());
+					product.setUnitPrice(request.getUnitPrice());
+					product.setImageUrl(request.getImageUrl());
+					product.setActive(request.isActive());
+					product.setUnitsInStock(request.getUnitsInStock());
+					
+					pdao.save(product);
+					return 1;
+				}
+				return 0;
+			}
 			
 }
+
+//class ProductRequest {
+//    private String sku;
+//
+//    private String name;
+//
+//    private BigDecimal unitPrice;
+//
+//    private String imageUrl;
+//
+//    private boolean active;
+//
+//    private int unitsInStock;
+//
+//	public String getSku() {
+//		return sku;
+//	}
+//
+//	public void setSku(String sku) {
+//		this.sku = sku;
+//	}
+//
+//	public String getName() {
+//		return name;
+//	}
+//
+//	public void setName(String name) {
+//		this.name = name;
+//	}
+//
+//	public BigDecimal getUnitPrice() {
+//		return unitPrice;
+//	}
+//
+//	public void setUnitPrice(BigDecimal unitPrice) {
+//		this.unitPrice = unitPrice;
+//	}
+//
+//	public String getImageUrl() {
+//		return imageUrl;
+//	}
+//
+//	public void setImageUrl(String imageUrl) {
+//		this.imageUrl = imageUrl;
+//	}
+//
+//	public boolean isActive() {
+//		return active;
+//	}
+//
+//	public void setActive(boolean active) {
+//		this.active = active;
+//	}
+//
+//	public int getUnitsInStock() {
+//		return unitsInStock;
+//	}
+//
+//	public void setUnitsInStock(int unitsInStock) {
+//		this.unitsInStock = unitsInStock;
+//	}
+//    
+//}
